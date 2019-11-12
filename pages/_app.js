c84ede7eb15114b4de3645e803fbedc8d402cab6
Gui <guilhermervcoelho@gmail.com>
2019-11-11 22:21:44 -0300

    app: fetch spreadsheetData

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import NextApp from 'next/app'
+import fetch from 'isomorphic-fetch'
 import Theme from '../components/Theme'
+
+const SPREADSHEET_URL = 'https://spreadsheets.google.com/feeds/cells/1F5PSFy5tjWltWVfI0RFmgRgHTn_lyED4Opo_28cIjm8/1/public/full?alt=json'
 
 class App extends NextApp {
   static async getInitialProps ({ Component, ctx }) {
@@ -7,6 +10,10 @@ class App extends NextApp {
       ? await Component.getInitialProps(ctx)
       : {}
 
+    const spreadsheetData = await fetch(SPREADSHEET_URL)
+      .then(res => res.json())
+
+    pageProps.spreadsheetData = spreadsheetData
     pageProps.query = ctx.query
 
     return { pageProps }
