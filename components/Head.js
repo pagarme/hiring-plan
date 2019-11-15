1c9f5d30536d1e82bdb514e4b33589ca978f65e8
Gui <guilhermervcoelho@gmail.com>
2019-11-15 12:19:51 -0300

    head: add favicon

--- components/Head.js
+++ components/Head.js
@@ -5,6 +5,7 @@ const Head = ({ children }) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700&display=swap" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
     <title>Hiring Plan</title>
     {children}
   </NextHead>
